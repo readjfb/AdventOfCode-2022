b09a13a0bede3f158e2972c6f90d18cd714d8ad9
readjfb <15671813+readjfb@users.noreply.github.com>
2022-12-01 00:05:44 -0600

    Solution 2 for day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,6 @@
+# https://adventofcode.com/2022/day/1
+
+
 def day1_v1():
     with open("day1_input.txt") as f:
         data = f.read().splitlines()
@@ -16,6 +19,26 @@ def day1_v1():
 
     print(f"Part 2: {sum(elves[:3])}")
 
+
+def day1_v2():
+    with open("day1_input.txt") as f:
+        data = f.read()
+
+    split_data = data.split("\n\n")
+
+    elves = []
+
+    for elf in split_data:
+        elves.append(sum(int(i) for i in elf.split("\n")))
+
+    print(f"Part 1: {max(elves)}")
+
+    elves.sort(reverse=True)
+
+    print(f"Part 2: {sum(elves[:3])}")
+
+
 if __name__ == "__main__":
-    pass
-day1_v1()
+    # day1_v1()
+
+    day1_v2()
